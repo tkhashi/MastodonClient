b064c5ae2513df3294303b09fe760cff9263611a
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-09-26 00:57:40 +0900

    Add MainFragment

---
+++ app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
@@ -0,0 +1,24 @@
+package io.keiji.sample.mastodonclient
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null){
+            val fragment = MainFragment()
+            supportFragmentManager.beginTransaction()
+                .add(
+                    R.id.fragment_container,
+                    fragment,
+                    MainFragment::class.java.simpleName
+                )
+                .commit()
+        }
+
+    }
+}
