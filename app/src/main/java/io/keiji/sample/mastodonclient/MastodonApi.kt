2df59646520aff2279c849dff2b748690b06205e
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-09-30 17:22:43 +0900

    Change emulator version31→28

---
+++ app/src/main/java/io/keiji/sample/mastodonclient/MastodonApi.kt
@@ -0,0 +1,13 @@
+package io.keiji.sample.mastodonclient
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface MastodonApi {
+
+    @GET("api/v1/timelines/public")
+    fun fetchPublicTimeline(
+    ): Call<ResponseBody>
+}
