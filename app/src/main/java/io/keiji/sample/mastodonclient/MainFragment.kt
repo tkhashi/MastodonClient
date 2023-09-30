92479bf1abde0f69ed1490988e4e69d6bceb6b22
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-09-30 17:51:32 +0900

    GET_MD_API

--- app/src/main/java/io/keiji/sample/mastodonclient/MainFragment.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/MainFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import io.keiji.sample.mastodonclient.databinding.FragmentMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 
 class MainFragment : Fragment (R.layout.fragment_main) {
     companion object {
         private val TAG = MainFragment::class.java.simpleName
-        private const val API_BASE_URL = "https://androidbook2020.keiji.io"
+        private const val API_BASE_URL = "https://social.vivaldi.net"
     }
 
     private val retrofit = Retrofit.Builder()
@@ -27,9 +31,14 @@ class MainFragment : Fragment (R.layout.fragment_main) {
         binding = DataBindingUtil.bind(view)
         binding?.button?.setOnClickListener {
             binding?.button?.text = "clicked"
-            val response = api.fetchPublicTimeline()
-                .execute().body()?.string().toString()
-            Log.d(TAG, response)
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = api.fetchPublicTimeline().string()
+                Log.d(TAG, response)
+                withContext(Dispatchers.Main){
+                    binding?.button?.text = response
+                }
+            }
         }
     }
 
