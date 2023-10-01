1b753e926f27aa283a8936157de368380dc6ffd3
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-10-01 11:25:56 +0900

    Dispaly image

--- app/src/main/java/io/keiji/sample/mastodonclient/DataBindingAdapter.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/DataBindingAdapter.kt
@@ -1,8 +1,10 @@
 package io.keiji.sample.mastodonclient
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter("spannedContent")
 fun TextView.setSpannedString(content: String) {
@@ -10,4 +12,16 @@ fun TextView.setSpannedString(content: String) {
         content,
         HtmlCompat.FROM_HTML_MODE_COMPACT
     )
+}
+
+@BindingAdapter("media")
+fun ImageView.setMedia(media: Media?){
+    if (media == null){
+        setImageDrawable(null)
+        return
+    }
+
+    Glide.with(this)
+        .load(media.url)
+        .into(this)
 }
--- app/src/main/java/io/keiji/sample/mastodonclient/Toot.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/Toot.kt
@@ -6,6 +6,11 @@ data class Toot(
     val id: String,
     @Json(name="created_at") val createdAt: String,
     val sensitive: Boolean,
+    val url: String,
+    @Json(name = "media_attachments") val mediaAttachments: List<Media>,
     val content: String,
     val account: Account
-)
+) {
+    val topMedia: Media?
+        get() = mediaAttachments.firstOrNull()
+}
