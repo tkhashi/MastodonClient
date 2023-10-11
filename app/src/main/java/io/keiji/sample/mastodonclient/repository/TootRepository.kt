54d2190832d28475b797c6a2d058cb25bc20538f
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-10-11 23:09:39 +0900

    Imp post toot

--- app/src/main/java/io/keiji/sample/mastodonclient/repository/TootRepository.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/repository/TootRepository.kt
@@ -3,11 +3,13 @@ package io.keiji.sample.mastodonclient.repository
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.keiji.sample.mastodonclient.MastodonApi
+import io.keiji.sample.mastodonclient.entity.Toot
 import io.keiji.sample.mastodonclient.entity.UserCredential
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.sql.ClientInfoStatus
 
 class TootRepository(
     private val userCredential: UserCredential
@@ -38,6 +40,15 @@ class TootRepository(
         api.fetchHomeTimeline(
             accessToken = "Bearer ${userCredential.accessToken}",
             maxId = maxId
+        )
+    }
+
+    suspend fun postToot(
+        status: String
+    ): Toot = withContext(Dispatchers.IO){
+        return@withContext api.postToot(
+            "Bearer ${userCredential.accessToken}",
+            status
         )
     }
 }
--- app/src/main/java/io/keiji/sample/mastodonclient/ui/edit/TootEditViewModel.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/ui/edit/TootEditViewModel.kt
@@ -3,7 +3,10 @@ package io.keiji.sample.mastodonclient.ui.edit
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import io.keiji.sample.mastodonclient.repository.TootRepository
+import io.keiji.sample.mastodonclient.repository.UserCredentialRepository
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class TootEditViewModel(
     private val instanceUrl: String,
@@ -11,6 +14,36 @@ class TootEditViewModel(
     private val coroutineScope: CoroutineScope,
     application: Application
 ) : AndroidViewModel(application){
+
+    private val userCredentialRepository = UserCredentialRepository(
+        application
+    )
+
     var status = MutableLiveData<String>()
 
+    val postComplete = MutableLiveData<Boolean>()
+    val errorMessasge = MutableLiveData<String>()
+
+    fun postToot(){
+        println("status: ${status}")
+        println("status.value: ${status.value}")
+        val statusSnapshot = status.value ?: return
+        if (statusSnapshot.isBlank()){
+            errorMessasge.postValue("投稿内容がありません")
+            return
+        }
+
+        coroutineScope.launch {
+            val credential = userCredentialRepository.find(instanceUrl, username)
+            if (credential == null){
+                return@launch
+            }
+
+            val tootRepository = TootRepository(credential)
+            tootRepository.postToot(
+                statusSnapshot
+            )
+            postComplete.postValue(true)
+        }
+    }
 }
